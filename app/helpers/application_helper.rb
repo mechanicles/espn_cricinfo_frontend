2d20badd399a202409194f0d223d640640fcafcf
Santosh Wadghule <santosh.wadghule@gmail.com>
2016-09-09 14:51:02 +0530

    Improvisation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def formatted_match_end_result(end_result)
+    return "" if end_result.blank?
+
+    if end_result != "tied"
+      "#{end_result} this match"
+    else
+      "Match got tied"
+    end
+  end
+
 end
