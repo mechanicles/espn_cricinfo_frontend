0eb736a9c5801e9c4fdecdafa8c90f543a3a9f6f
Santosh Wadghule <santosh.wadghule@gmail.com>
2016-09-11 12:56:44 +0530

    Move URL to the ENV variable.

--- lib/espn_crickinfo_service.rb
+++ lib/espn_crickinfo_service.rb
@@ -1,19 +1,17 @@
 class EspnCrickinfoService
 
   def get_all_maches_info
-    @all_matches ||= JSON.parse(Http.get("#{main_espn_crick_info_url}/matches.json").body)
+    @all_matches ||= JSON.parse(Http.get("#{backend_espncrickinfo_url}/matches.json").body)
   end
 
   def get_match_info(match_id)
-    @match ||= JSON.parse(Http.get("#{main_espn_crick_info_url}/matches/#{match_id}.json").body)
+    @match ||= JSON.parse(Http.get("#{backend_espncrickinfo_url}/matches/#{match_id}.json").body)
   end
 
   private
 
-  def main_espn_crick_info_url
-    # Temporarily added here but this needs to be moved into settings.yml file.
-
-    "http://localhost:3000"
+  def backend_espncrickinfo_url
+    @url ||= ENV["BACKEND_ESPNCRICKINFO_URL"]
   end
 
 end
