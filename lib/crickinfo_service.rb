8ce23ebe9b69aa711e90ee71e0a52bdfdfefb1e2
Santosh Wadghule <santosh.wadghule@gmail.com>
2020-03-13 22:04:15 +0530

    Working

--- lib/crickinfo_service.rb
+++ lib/crickinfo_service.rb
@@ -1,15 +1,26 @@
+# frozen_string_literal: true
+
+require 'net/http'
+
 class CrickinfoService
   def get_all_matches_info
-    JSON.parse(Http.get("#{backend_espncrickinfo_url}/matches.json").body)["all_matches"]
+    uri = parse_uri("#{backend_espncrickinfo_url}/matches.json")
+    JSON.parse(Net::HTTP.get(uri))['all_matches']
   end
 
   def get_match_info(match_id)
-    JSON.parse Http.get("#{backend_espncrickinfo_url}/matches/#{match_id}.json").body
+    uri = parse_uri("#{backend_espncrickinfo_url}/matches/#{match_id}.json")
+    JSON.parse(Net::HTTP.get(uri))
   end
 
   private
 
   def backend_espncrickinfo_url
-    ENV["BACKEND_ESPNCRICKINFO_URL"]
+    ENV['BACKEND_ESPNCRICKINFO_URL']
+  end
+
+  def parse_uri(uri_address)
+    escaped_address = URI.escape(uri_address)
+    URI.parse(escaped_address)
   end
 end
